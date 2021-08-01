ee368087867140faee1bbd75489094bfa0877173
Nikita Veselov <niki4.1995@gmail.com>
2021-08-01 14:13:40 +0300

    add google ads

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,16 @@
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet" type="text/css" >
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
+    
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4TLD1DNGJ2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4TLD1DNGJ2');
+        </script>
     </head>
     <body>
         
