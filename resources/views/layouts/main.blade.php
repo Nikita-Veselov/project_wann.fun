73bfbd052aafc1d08e5c21949bd89d9a57f817ed
Nikita Veselov <niki4.1995@gmail.com>
2021-08-08 20:55:11 +0300

    addition change for main panel width

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -169,9 +169,9 @@
         <!-- Main-->
         <header class="masthead" style="background: url('{{ asset('assets/img/bg-masthead.jpg') }}') no-repeat center center"> 
             
-            <div class="container position-relative">   
+            <div class="container-fluid position-relative">   
                 <div class="row justify-content-center">
-                    <div class="col-xl-6">
+                    <div class="col px-4 col-lg-6 px-lg-0 ">
                         <div class="text-center text-white">
                             <!-- Page heading-->
                             <h1 class="custom-font mb-5">Create your new short URL!</h1>
