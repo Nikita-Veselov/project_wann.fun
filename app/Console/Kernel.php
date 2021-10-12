a4b29ea3b0bffea4ada982f1c8a624567372e292
plus <54999793+Nikita-Veselov@users.noreply.github.com>
2021-10-12 21:52:15 +0300

    testing cron tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,7 @@ use DateTime;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\VarDumper\Caster\LinkStub;
 
 class Kernel extends ConsoleKernel
@@ -29,11 +30,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            Link::where('updated_at', '<', Carbon::now()->subDays(14))
-                ->where('user_id', '=', 'guest')
-                ->delete();
-        })->daily();
+        Log::info("Working");
+        // $schedule->call(function () {
+        //     Link::where('updated_at', '<', Carbon::now()->subDays(14))
+        //         ->where('user_id', '=', 'guest')
+        //         ->delete();
+        // })->daily();
     }
 
     /**
