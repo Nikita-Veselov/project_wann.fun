498b7505ec9bd4b40503a270ca006eddcdce4b66
plus <54999793+Nikita-Veselov@users.noreply.github.com>
2021-10-12 22:25:25 +0300

    testing cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,13 +4,8 @@ namespace App\Console;
 
 use App\Models\Link;
 use Carbon\Carbon;
-use DateTime;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\Log;
-use Symfony\Component\VarDumper\Caster\LinkStub;
-
 class Kernel extends ConsoleKernel
 {
     /**
@@ -35,7 +30,7 @@ class Kernel extends ConsoleKernel
             Link::where('updated_at', '<', Carbon::now()->subDays(14))
                 ->where('user_id', '=', 'guest')
                 ->delete();
-        })->daily();
+        })->everyFiveMinutes();
     }
 
     /**
